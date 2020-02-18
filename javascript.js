a46a15cb45c6de37dc7955c565afa0679c80f8da
John Spruijt <99058314@mydavinci.nl>
2020-02-18 11:12:21 +0100

    added scroll function , projects | changed nav

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
-function scroll(id) {
+function scrollToThing(id) {
     document.getElementById(id).scrollIntoView({behavior: "smooth"});
+}
+function scrollToTop() {
+    scrollTo(0,0)
 }
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
@@ -7,12 +10,9 @@ window.onscroll = function() {myFunction()};
 // Get the navbar
 var navbar = document.getElementById("nav");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= 1) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
